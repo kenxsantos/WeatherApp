f9c95f068973a0d3d81e6a15c95fd23a92662d9a
kenxsantos <kenxsantos@gmail.com>
2025-03-21 18:36:04 +0800

    create a usable component

---
+++ lib/component/icon_text_column.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class IconTextColumn extends StatelessWidget {
+  final IconData icon;
+  final String text1;
+  final String text2;
+  final VoidCallback onPressed;
+
+  const IconTextColumn({
+    super.key,
+    required this.icon,
+    required this.text1,
+    required this.text2,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          icon: Icon(icon),
+          color: Colors.white,
+          iconSize: 32,
+          onPressed: onPressed,
+        ),
+        Text(
+          text1,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+            color: const Color.fromARGB(221, 255, 255, 255),
+          ),
+        ),
+        Text(
+          text2,
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+            color: const Color.fromARGB(221, 139, 138, 138),
+          ),
+        ),
+      ],
+    );
+  }
+}
