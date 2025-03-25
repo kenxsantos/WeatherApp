99dc9342271508c1ad27af9ece8dc5a62e828124
kenxsantos <kenxsantos@gmail.com>
2025-03-25 11:30:32 +0800

    refactor IconTextColumn component and update MyApp to support theme switching

--- lib/components/icon_text_column.dart
+++ lib/components/icon_text_column.dart
@@ -4,27 +4,28 @@ class IconTextColumn extends StatelessWidget {
   final IconData icon;
   final String text1;
   final String text2;
-  final VoidCallback onPressed;
 
   const IconTextColumn({
     super.key,
     required this.icon,
     required this.text1,
     required this.text2,
-    required this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        IconButton(
-          icon: Icon(icon),
-          color: Colors.white,
-          iconSize: 24,
-          onPressed: onPressed,
+        Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+          child: Icon(
+            icon,
+            size: 24,
+            color: const Color.fromARGB(221, 255, 255, 255),
+          ),
         ),
-
         Text(
           text1,
           textAlign: TextAlign.start,
@@ -34,7 +35,6 @@ class IconTextColumn extends StatelessWidget {
             color: const Color.fromARGB(221, 255, 255, 255),
           ),
         ),
-
         Text(
           text2,
           textAlign: TextAlign.start,
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:weatherapp/screens/home_page.dart';
 import 'package:weatherapp/viewmodels/home_viewmodel_provider.dart';
 
 void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  _MyAppState createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.system;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-      ),
-      home: HomeViewModelProvider(),
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeMode,
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
+  }
+
+  void changeTheme(ThemeMode themeMode) {
+    setState(() {
+      themeMode = themeMode;
+    });
   }
 }
